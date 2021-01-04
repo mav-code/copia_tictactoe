65aa79a59423f1a7890780aa036a98fab2ef8b6e
mav-code <mavisansmembres@gmail.com>
2021-01-04 16:04:54 -0500

    toggle turns & loop

--- lib/interface.rb
+++ lib/interface.rb
@@ -9,4 +9,8 @@ end
 
 def turn
     puts "Player #{$player} -- enter row & column of your desired move"
+    play = gets.chomp
+    $board[play[0].to_i][play[2].to_i] = $player
+    $player == 'X' ?  $player = 'O' : $player = 'X'
+    turn
 end
