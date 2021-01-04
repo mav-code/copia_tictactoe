57db12b2e3469cfee516a7e7ea707d69d960546e
mav-code <mavisansmembres@gmail.com>
2021-01-04 16:18:57 -0500

    print board to specifications

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,6 +1,6 @@
 def newgame
     system "clear"
-    $board = [['','',''],['','',''],['','','']]
+    $board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
     $player = 'X'
     puts "CLI Tic-Tac-Toe"
     puts "by Mavi"
@@ -8,9 +8,18 @@ def newgame
 end
 
 def turn
-    puts "Player #{$player} -- enter row & column of your desired move"
+    puts "Player #{$player} -- enter row & column of your desired move, separated by a space"
     play = gets.chomp
-    $board[play[0].to_i][play[2].to_i] = $player
+    $board[play[0].to_i - 1][play[2].to_i - 1] = $player ### string converted into 0-indexed integers
+    printBoard
     $player == 'X' ?  $player = 'O' : $player = 'X'
     turn
+end
+
+def printBoard
+    puts "\n"
+    puts "| #{$board[0][0]} | #{$board[0][1]} | #{$board[0][2]} |"
+    puts "| #{$board[1][0]} | #{$board[1][1]} | #{$board[1][2]} |"
+    puts "| #{$board[2][0]} | #{$board[2][1]} | #{$board[2][2]} |"
+    puts "\n"
 end
