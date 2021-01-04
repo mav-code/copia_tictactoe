a7395c920138ec51eb5319993e2fffeceb0f9f73
mav-code <mavisansmembres@gmail.com>
2021-01-04 15:53:45 -0500

    turn prompt

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,5 +1,12 @@
 def newgame
     system "clear"
+    $board = [['','',''],['','',''],['','','']]
+    $player = 'X'
     puts "CLI Tic-Tac-Toe"
     puts "by Mavi"
+    turn
+end
+
+def turn
+    puts "Player #{$player} -- enter row & column of your desired move"
 end
